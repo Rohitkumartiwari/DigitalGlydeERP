ee71d9f6d96d26922ea44b9cab1278293b7eeb69
Rohitkumartiwari <rohittiwari.5july@gmail.com>
2021-10-06 11:12:33 +0530

    Create Projectdoctab.js

---
+++ src/tabs/Projectdoctab.js
@@ -0,0 +1,10 @@
+import React from "react";
+ const Projectdatatab=()=>
+ {
+     return(
+         <h1>
+            project document tab
+         </h1>
+     )
+ }
+ export default Projectdatatab;
