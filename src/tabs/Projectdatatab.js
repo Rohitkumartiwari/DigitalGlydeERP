035db9b27742eb3e73c850213466e8931c9035f8
Rohitkumartiwari <rohittiwari.5july@gmail.com>
2021-10-12 15:30:42 +0530

    Update Projectdatatab.js

--- src/tabs/Projectdatatab.js
+++ src/tabs/Projectdatatab.js
@@ -1,8 +1,10 @@
 import React from "react";
+
  const Projectdatatab=(props)=>
  {
      console.log(props.location.state.data);
      return(
+         <>
          <h1>
               Project name is --{props.location.state.data.pname}
           <br></br>
@@ -10,11 +12,10 @@ import React from "react";
             <br></br>
             Current task--{props.location.state.data.task}
             <br></br>
-             task desc--{props.location.state.data.taskdisc}
-            <br></br>
              task status--{props.location.state.data.status}
             
          </h1>
+         </>
      )
  }
  export default Projectdatatab;
